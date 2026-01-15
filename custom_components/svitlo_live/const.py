58637c44015a8b38ef41a71a0ea4245e3fa056da
Serhii Chaichuk <128085928+chaichuk@users.noreply.github.com>
2026-01-15 12:28:25 +0200

    Update const.py

--- custom_components/svitlo_live/const.py
+++ custom_components/svitlo_live/const.py
@@ -13,51 +13,55 @@ DEFAULT_SCAN_INTERVAL = 900  # 15 хв
 
 CONF_REGION = "region"
 CONF_QUEUE = "queue"
-CONF_SCAN_INTERVAL = "scan_interval"
-CONF_OPERATOR = "operator"  # <-- Нова константа для вибору оператора
+CONF_OPERATOR = "operator"
 
-# Оновлений список регіонів
+# --- 1. REGIONS: ЗАЛИШАЄМО СТАРІ КЛЮЧІ (це важливо для збереження історії) ---
 REGIONS = {
-    # --- Спеціальні регіони (через ваш проксі) ---
+    # Спеціальні
     "kyiv": "м. Київ",
-    "dnipro-city": "м. Дніпро",  # Віртуальний регіон для UI, далі розпадається на ДнЕМ/ЦЕК
+    "dnipro-city": "м. Дніпро", 
+
+    # Області (Старі ключі, як були у v2.x)
+    "harkivska-oblast": "Харківська область",      # Було harkivska
+    "hmelnitska-oblast": "Хмельницька область",    # Було hmelnitska
+    "chernigivska-oblast": "Чернігівська область", # Було chernigivska
+    "jitomirska-oblast": "Житомирська область",    # Було jitomirska
     
-    # Регіони, що підтримуються новим API
-    "kiivska-oblast": "Київська область",
-    "odeska-oblast": "Одеська область",
-    "dnipropetrovska-oblast": "Дніпропетровська область",
-    "lvivska-oblast": "Львівська область",
-    
-    "poltavska-oblast": "Полтавська область",
+    # Решта регіонів (ключі співпадають або не змінювались критично)
     "cherkaska-oblast": "Черкаська область",
-    "chernihivska-oblast": "Чернігівська область",
-    "kharkivska-oblast": "Харківська область",
-    "khmelnytska-oblast": "Хмельницька область",
+    "chernivetska-oblast": "Чернівецька область",
+    "dnipropetrovska-oblast": "Дніпропетровська область",
+    "donetska-oblast": "Донецька область",
     "ivano-frankivska-oblast": "Івано-Франківська область",
+    "kiivska-oblast": "Київська область",
+    "kirovogradska-oblast": "Кіровоградська область",
+    "lvivska-oblast": "Львівська область",
+    "mikolaivska-oblast": "Миколаївська область",
+    "odeska-oblast": "Одеська область",
+    "poltavska-oblast": "Полтавська область",
     "rivnenska-oblast": "Рівненська область",
+    "sumska-oblast": "Сумська область",
     "ternopilska-oblast": "Тернопільська область",
+    "vinnitska-oblast": "Вінницька область",
+    "volinska-oblast": "Волинська область",
     "zakarpatska-oblast": "Закарпатська область",
     "zaporizka-oblast": "Запорізька область",
-    "zhytomyrska-oblast": "Житомирська область",
-    
-    # Інші (поки що залишаємо старі ключі, якщо вони працюють через старе API, або коментуємо)
-    # "chernivetska-oblast": "Чернівецька область",
-    # "donetska-oblast": "Донецька область",
-    # "kirovogradska-oblast": "Кіровоградська область",
-    # "mikolaivska-oblast": "Миколаївська область",
-    # "sumska-oblast": "Сумська область",
-    # "vinnitska-oblast": "Вінницька область",
-    # "volinska-oblast": "Волинська область",
 }
 
-# Мапа режимів вибору черги/групи
+# --- 2. МАПА ПЕРЕКЛАДУ: Старий конфіг -> Новий API ---
+API_REGION_MAP = {
+    "harkivska-oblast": "kharkivska-oblast",
+    "hmelnitska-oblast": "khmelnytska-oblast",
+    "chernigivska-oblast": "chernihivska-oblast",
+    "jitomirska-oblast": "zhytomyrska-oblast",
+    # Можна додати інші, якщо помітите невідповідність
+}
+
 REGION_QUEUE_MODE = {
-    # "chernivetska-oblast": "GRUPA_NUM",
-    # "donetska-oblast": "GRUPA_NUM",
+    "chernivetska-oblast": "GRUPA_NUM",
+    "donetska-oblast": "GRUPA_NUM",
 }
 
-# Основний API (старий)
-API_URL = "https://svitlo-proxy.svitlo-proxy.workers.dev"
-
-# Персональний API (Cloudflare Worker)
-DTEK_API_URL = "https://dtek-api.svitlo-proxy.workers.dev/"
+# Використовуємо лише новий API
+API_URL = "https://dtek-api.svitlo-proxy.workers.dev/" 
+# Старий URL можна залишити як константу про всяк випадок, але логіка тепер йде через єдиний
